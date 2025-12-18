0c17f9da69dbe3ac4ea6737f1b60f70f7ec8e594
raghur4567 <iamraghu1802@gmail.com>
2025-12-18 05:30:26 +0000

    docker-compose-file-updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_URL = "http://localhost:8080/api/contacts";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
   const [contacts, setContacts] = useState([]);
